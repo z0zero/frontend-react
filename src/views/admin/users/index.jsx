fc4525ecd626aa40a3d3c9a2c498595cc4a838cc
z0zero <briansangapta@gmail.com>
2024-06-06 16:01:00 +0700

    chore: Update UsersIndex component to fetch and display users data

--- src/views/admin/users/index.jsx
+++ src/views/admin/users/index.jsx
@@ -1,10 +1,50 @@
+//import useState dan useEffect
+import { useState, useEffect } from "react";
+
 //import SidebarMenu
 import SidebarMenu from "../../../components/SidebarMenu";
 
 //import Link
 import { Link } from "react-router-dom";
 
+//import js cookie
+import Cookies from "js-cookie";
+
+//import api
+import api from "../../../services/api";
+
 export default function UsersIndex() {
+  //ini state "users"
+  const [users, setUsers] = useState([]);
+
+  //define method "fetchDataUsers"
+  const fetchDataUsers = async () => {
+    //get token from cookies inside the function to ensure it's up-to-date
+    const token = Cookies.get("token");
+
+    if (token) {
+      //set authorization header with token
+      api.defaults.headers.common["Authorization"] = token;
+
+      //fetch data from API with Axios
+      try {
+        const response = await api.get("/api/admin/users");
+        //assign response data to state "users"
+        setUsers(response.data.data);
+      } catch (error) {
+        console.error("There was an error fetching the users!", error);
+      }
+    } else {
+      console.error("Token is not available!");
+    }
+  };
+
+  //run hook useEffect
+  useEffect(() => {
+    //call method "fetchDataUsers"
+    fetchDataUsers();
+  }, []);
+
   return (
     <div className="container mt-5 mb-5">
       <div className="row">
@@ -22,7 +62,48 @@ export default function UsersIndex() {
                 ADD USER
               </Link>
             </div>
-            <div className="card-body">HALAMAN USERS INDEX</div>
+            <div className="card-body">
+              <table className="table table-bordered">
+                <thead className="bg-dark text-white">
+                  <tr>
+                    <th scope="col">Full Name</th>
+                    <th scope="col">Email Address</th>
+                    <th scope="col" style={{ width: "17%" }}>
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.length > 0 ? (
+                    users.map((user, index) => (
+                      <tr key={index}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td className="text-center">
+                          <Link
+                            to={`/admin/users/edit/${user.id}`}
+                            className="btn btn-sm btn-primary rounded-sm shadow border-0 me-2"
+                          >
+                            EDIT
+                          </Link>
+                          <button className="btn btn-sm btn-danger rounded-sm shadow border-0">
+                            DELETE
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="4" className="text-center">
+                        <div className="alert alert-danger mb-0">
+                          Data Belum Tersedia!
+                        </div>
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
