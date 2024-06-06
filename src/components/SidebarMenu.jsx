ad718bf57ca78913701397dc33f51526b4f6fd23
z0zero <briansangapta@gmail.com>
2024-06-06 15:38:15 +0700

    chore: Add logout functionality to SidebarMenu component

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,7 +1,35 @@
 //import Link from react router dom
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+//import hook react
+import React, { useContext } from "react";
+
+// import Cookies from js-cookie
+import Cookies from "js-cookie";
+
+//import context
+import { AuthContext } from "../context/AuthContext";
 
 export default function SidebarMenu() {
+  // Menggunakan hook `useNavigate` untuk navigasi
+  const navigate = useNavigate();
+
+  //destructure context "setIsAuthenticated"
+  const { setIsAuthenticated } = useContext(AuthContext);
+
+  // method to handle logout
+  const logout = () => {
+    //remove token and user on cookies
+    Cookies.remove("token");
+    Cookies.remove("user");
+
+    //assign false to state "isAuthenticated"
+    setIsAuthenticated(false);
+
+    // redirect to login
+    navigate("/login", { replace: true });
+  };
+
   return (
     <div className="card border-0 rounded shadow-sm">
       <div className="card-header">MAIN MENU</div>
@@ -21,7 +49,7 @@ export default function SidebarMenu() {
             Users
           </Link>
           <a
-            href="#"
+            onClick={logout}
             className="list-group-item list-group-item-action"
             style={{ cursor: "pointer" }}
           >
